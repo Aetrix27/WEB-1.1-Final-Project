f92ff95c49214c6743cb136060270be522028f01
Aetrix27 <davedogg323@gmail.com>
2023-09-20 17:28:13 -0700

    Fixed mongo issue

--- app.py
+++ app.py
@@ -1,217 +1,234 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
 from datetime import datetime
 import calendar
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 # Written with help from https://www.guru99.com/calendar-in-python.html
 
 ############################################################
 # SETUP
 ############################################################
 
-app = Flask(__name__)
-
-secret = os.getenv('MONGO_PASS')
-
-host = "mongodb+srv://david-323:"+ secret +"@cluster0.dqhub.mongodb.net/?retryWrites=true&w=majority"
-app.config["MONGO_URI"] = host
-mongo = PyMongo(app)
 
 
 ############################################################
 # ROUTES
 ############################################################
 
-@app.route('/')
-def events_list():
-    """Display the events list page."""
-    try:
-        today = datetime.today()
-        current_month = today.strftime('%B')
-        c = calendar.TextCalendar(calendar.SUNDAY)
-        days = c.itermonthdays(2025, today.month)
-        calendar_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
-        events_data = mongo.db.events.find({})
+def create_app():
 
-        context = {
-            'events': events_data,
-            'c': c,
-            'calendar_days': calendar_days,
-            'current_month': current_month,
-            'days': days,
-            'eventsPresent': True
+    app = Flask(__name__)
+    load_dotenv()
 
-        }
+    #secret = os.getenv('MONGO_PASS')
+    #secret = os.getenv('SECRET_KEY')
+    host = os.environ.get('MONGO_URI')
+    app.config["MONGO_URI"] = host
 
-        return render_template('events_list.html', **context)
-    except Exception as e:
-        today = datetime.today()
-        current_month = today.strftime('%B')
-        c = calendar.TextCalendar(calendar.SUNDAY)
-        days = c.itermonthdays(2025, today.month)
-        calendar_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+    mongo = PyMongo(app)
 
-        context = {
-            'c': c,
-            'calendar_days': calendar_days,
-            'current_month': current_month,
-            'days': days,
-            'eventsPresent': False
+    
+# Send a ping to confirm a successful connection
 
-        }
-
-        return render_template('events_list.html', **context)
+    @app.route('/')
+    def events_list():
+        """Display the events list page."""
+        try:
+            today = datetime.today()
+            current_month = today.strftime('%B')
+            c = calendar.TextCalendar(calendar.SUNDAY)
+            days = c.itermonthdays(2025, today.month)
+            calendar_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+            if events_data != {}:
+                events_data = mongo.db.events.find({})
+                print(mongo.events.find({}))
 
 
-@app.route('/profile')
-def about():
-    """Display the profile page."""
+            context = {
+                'events': events_data,
+                'c': c,
+                'calendar_days': calendar_days,
+                'current_month': current_month,
+                'days': days,
+                'eventsPresent': True
 
-    return render_template('detail.html')
+            }
 
+            return render_template('events_list.html', **context)
+        except Exception as e:
+            today = datetime.today()
+            current_month = today.strftime('%B')
+            c = calendar.TextCalendar(calendar.SUNDAY)
+            days = c.itermonthdays(2025, today.month)
+            calendar_days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
 
-@app.route('/create/<in_day>', methods=['GET', 'POST'])
-def create(in_day):
-    """Display the event creation page & process data from the event form."""
-    event = request.form.get('event_name')
-    photo_url = request.form.get('photo_url')
-    location = request.form.get('location')
-    description = request.form.get('description')
+            context = {
+                'c': c,
+                'calendar_days': calendar_days,
+                'current_month': current_month,
+                'days': days,
+                'eventsPresent': False
 
-    if request.method == 'POST':
-        date_created = request.form.get('date_created')
-        date_obj = datetime.strptime(date_created, '%Y-%m-%d')
-        date_in_days = date_obj.strftime('%-d')
+            }
 
-        new_event = {
-            'event_name': event,
-            'photo_url': photo_url,
-            'day_chosen': int(date_in_days),
-            'date_created': date_created,
-            'location': location,
-            'description': description
-        }
-
-        result = mongo.db.events.insert_one(new_event)
-        inserted_id = result.inserted_id
-
-        return redirect(url_for('detail', event_id=inserted_id))
-    else:
-        print(in_day)
-        if int(in_day) < 10:
-            date = datetime.now()
-            date=date.replace(day=int(in_day))
-            date.strftime(f'%Y-%m-%d')
-        elif int(in_day) >= 10:
-            date = datetime.now()
-            date=date.replace(day=int(in_day))
-            date.strftime(f'%Y-%m-%d')
-
-        context = {
-            'day_in': date
-
-        }
-
-        return render_template('create.html', **context)
+            return render_template('events_list.html', **context)
 
 
-@app.route('/event/<event_id>')
-def detail(event_id):
-    """Display the event detail page & process data from the profile form."""
+    @app.route('/profile')
+    def about():
+        """Display the profile page."""
 
-    event_to_show = mongo.db.events.find_one({
-        "_id": ObjectId(event_id)
-    })
-
-    profiles = mongo.db.profiles.find({
-        "event_id": ObjectId(event_id)
-    })
-
-    context = {
-        'event': event_to_show,
-        'profiles': profiles,
-        'event_id': ObjectId(event_id)
-
-    }
-
-    return render_template('detail.html', **context)
+        return render_template('detail.html')
 
 
-@app.route('/profile/<event_id>', methods=['POST'])
-def profile(event_id):
-    """Display the event detail page & process data from the profile form."""
-
-    date_created = request.form.get("date_created")
-    profile_name = request.form.get("profile_name")
-    number_attending = request.form.get("number_attending")
-    time = request.form.get("time")
-
-    new_profile = {
-        'number_attending': number_attending,
-        'date_created': date_created,
-        'event_id': ObjectId(event_id),
-        'profile_name': profile_name,
-        'time': time
-
-    }
-
-    mongo.db.profiles.insert_one(new_profile)
-
-    return redirect(url_for('detail', event_id=event_id))
-
-
-@app.route('/edit/<event_id>', methods=['GET', 'POST'])
-def edit(event_id):
-    """Shows the edit page and accepts a POST request with edited data."""
-    if request.method == 'POST':
+    @app.route('/create/<in_day>', methods=['GET', 'POST'])
+    def create(in_day):
+        """Display the event creation page & process data from the event form."""
         event = request.form.get('event_name')
         photo_url = request.form.get('photo_url')
-        date_created = request.form.get('date_created')
         location = request.form.get('location')
         description = request.form.get('description')
 
-        mongo.db.events.update_one({
-            '_id': ObjectId(event_id)
-        },
-            {
-            '$set': {
-                '_id': ObjectId(event_id),
+        if request.method == 'POST':
+            date_created = request.form.get('date_created')
+            date_obj = datetime.strptime(date_created, '%Y-%m-%d')
+            date_in_days = date_obj.strftime('%d')
+
+            new_event = {
                 'event_name': event,
-                'date_created': date_created,
                 'photo_url': photo_url,
+                'day_chosen': int(date_in_days),
+                'date_created': date_created,
                 'location': location,
                 'description': description
             }
+
+            result = mongo.db.events.insert_one(new_event)
+            inserted_id = result.inserted_id
+
+            return redirect(url_for('detail', event_id=inserted_id))
+        else:
+            if int(in_day) < 10:
+                date = datetime.now()
+                date=date.replace(day=int(in_day))
+                date.strftime(f'%Y-%m-%d')
+            elif int(in_day) >= 10:
+                date = datetime.now()
+                date=date.replace(day=int(in_day))
+                date.strftime(f'%Y-%m-%d')
+
+            context = {
+                'day_in': date
+
+            }
+
+            return render_template('create.html', **context)
+
+
+    @app.route('/event/<event_id>')
+    def detail(event_id):
+        """Display the event detail page & process data from the profile form."""
+
+        event_to_show = mongo.db.events.find_one({
+            "_id": ObjectId(event_id)
         })
 
-        return redirect(url_for('detail', event_id=event_id))
-    else:
-        event_to_show = mongo.db.events.find_one({
-            '_id': ObjectId(event_id)
+        profiles = mongo.db.profiles.find({
+            "event_id": ObjectId(event_id)
         })
 
         context = {
-            'event': event_to_show
+            'event': event_to_show,
+            'profiles': profiles,
+            'event_id': ObjectId(event_id)
+
         }
 
-        return render_template('edit.html', **context)
+        return render_template('detail.html', **context)
 
 
-@app.route('/delete/<event_id>', methods=['POST'])
-def delete(event_id):
-    mongo.db.events.delete_one({
-        '_id': ObjectId(event_id)
-    })
-    mongo.db.profiles.delete_many({
-        'event_id': ObjectId(event_id)
-    })
+    @app.route('/profile/<event_id>', methods=['POST'])
+    def profile(event_id):
+        """Display the event detail page & process data from the profile form."""
 
-    return redirect(url_for('events_list'))
+        date_created = request.form.get("date_created")
+        profile_name = request.form.get("profile_name")
+        number_attending = request.form.get("number_attending")
+        time = request.form.get("time")
+
+        new_profile = {
+            'number_attending': number_attending,
+            'date_created': date_created,
+            'event_id': ObjectId(event_id),
+            'profile_name': profile_name,
+            'time': time
+
+        }
+
+        mongo.db.profiles.insert_one(new_profile)
+
+        return redirect(url_for('detail', event_id=event_id))
+
+
+    @app.route('/edit/<event_id>', methods=['GET', 'POST'])
+    def edit(event_id):
+        """Shows the edit page and accepts a POST request with edited data."""
+        if request.method == 'POST':
+            event = request.form.get('event_name')
+            photo_url = request.form.get('photo_url')
+            date_created = request.form.get('date_created')
+            location = request.form.get('location')
+            description = request.form.get('description')
+
+            mongo.db.events.update_one({
+                '_id': ObjectId(event_id)
+            },
+                {
+                '$set': {
+                    '_id': ObjectId(event_id),
+                    'event_name': event,
+                    'date_created': date_created,
+                    'photo_url': photo_url,
+                    'location': location,
+                    'description': description
+                }
+            })
+
+            return redirect(url_for('detail', event_id=event_id))
+        else:
+            event_to_show = mongo.db.events.find_one({
+                '_id': ObjectId(event_id)
+            })
+
+            context = {
+                'event': event_to_show
+            }
+
+            return render_template('edit.html', **context)
+
+
+    @app.route('/delete/<event_id>', methods=['POST'])
+    def delete(event_id):
+        mongo.db.events.delete_one({
+            '_id': ObjectId(event_id)
+        })
+        mongo.db.profiles.delete_many({
+            'event_id': ObjectId(event_id)
+        })
+
+        return redirect(url_for('events_list'))
+
+
+    return app
+
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
